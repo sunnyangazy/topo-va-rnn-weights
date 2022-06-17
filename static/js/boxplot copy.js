e4195a0425c1a0c1c757a62b0b6cdc3c9c2a5f0d
sunny <1286673795@qq.com>
2022-06-17 20:33:51 +0800

    first commit

---
+++ static/js/boxplot copy.js
@@ -0,0 +1,18 @@
+var y0 = a;
+var y1 = b;
+
+
+var trace1 = {
+  y: y0,
+  type: 'box'
+};
+
+var trace2 = {
+  y: y1,
+  type: 'box'
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('boxChart', data);
+
